3dea582520a56296cd0bef99f1a6cd35dcfe3c80
Vuong Hong An <anvh87@gmail.com>
2020-05-05 23:22:03 +0700

    Initial commit

---
+++ migrations/20171125081128-create-user.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      isMale: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    }).then(() => queryInterface.addIndex('Users', ['isMale'], {
+      name: 'Users_isMale_idx'
+    }));
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
